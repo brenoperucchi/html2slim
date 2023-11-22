a780ef710e22eabce3d6f2a9b581a46216c708c8
Breno Perucchi <bperucchi@gmail.com>
2023-11-22 18:04:41 -0300

    Fixing remove break lines

--- lib/html2slim/converter.rb
+++ lib/html2slim/converter.rb
@@ -12,12 +12,28 @@ module HTML2Slim
     end
   end
   class ERBConverter < Converter
+
+    def remove_break_lines(erb)
+      count_brackets = 0
+      erb_modified = erb.each_line.map do |line|
+        count_brackets += line.count("(") - line.count(")")
+        if count_brackets > 0
+          line.gsub!(/\s+/, '')  # Remove all whitespace
+          line.gsub!(/,/, ', ')  # Add a space after each comma
+          line.chomp!            # Remove the newline at the end
+        end
+        line
+      end.join
+      erb_modified
+    end
+
     def initialize(file)
       # open.read makes it works for files & IO
       erb = File.exist?(file) ? open(file).read : file
 
-      erb.gsub!(/<%(.+?)\s*\{\s*(\|.+?\|)?\s*%>/){ %(<%#{$1} do #{$2}%>) }
+      erb = remove_break_lines(erb)
 
+      erb.gsub!(/<%(.+?)\s*\{\s*(\|.+?\|)?\s*%>/){ %(<%#{$1} do #{$2}%>) }
       # case, if, for, unless, until, while, and blocks...
       erb.gsub!(/<%(-\s+)?((\s*(case|if|for|unless|until|while) .+?)|.+?do\s*(\|.+?\|)?\s*)-?%>/){ %(<ruby code="#{$2.gsub(/"/, '&quot;')}">) }
       # else
